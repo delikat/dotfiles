e3f07ecf10acaed0c2dcea6156a5e5d431957c34
Andrew Delikat <andrew@delikat.moe>
2024-11-07 02:39:53 -0800

    nvm

--- main.sh
+++ main.sh
@@ -22,5 +22,16 @@ warn() {
 
 info "####### dotfiles #######"
 
+# misc stuff to be split into more appropriate files
+
+## nvm
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/HEAD/install.sh | bash
+source ~/.zshrc
+nvm install --lts
+nvm use --lts
+NODE_LTS_VERSION="$(nvm current)"
+nvm alias default $NODE_LTS_VERSION
+npm install npm --global
+
 . brew.sh
 . macos.sh
